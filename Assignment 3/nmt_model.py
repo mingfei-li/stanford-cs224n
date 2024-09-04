01cd9118fdd3749422f86f9d64ad95775e42380f
Mingfei Li <ml@mingfei.io>
2024-09-03 17:51:09 -0700

    Update nmt_model.py

--- Assignment 3/nmt_model.py
+++ Assignment 3/nmt_model.py
@@ -369,7 +369,7 @@ class NMT(nn.Module):
         ###     Tanh:
         ###         https://pytorch.org/docs/stable/generated/torch.tanh.html
 
-        alpha_t = nn.functional.softmax(e_t)
+        alpha_t = F.softmax(e_t, dim=-1)
         a_t = torch.bmm(alpha_t.unsqueeze(1), enc_hiddens).squeeze(1)
         U_t = torch.cat((a_t, dec_hidden), dim=-1)
         V_t = self.combined_output_projection(U_t)
