fixed bug about applying attention mask

Final project/bert.py
@@ -48,10 +48,9 @@ class BertSelfAttention(nn.Module):
     # - Multiply the attention scores with the value to get back weighted values.
     # - Before returning, concatenate multi-heads to recover the original shape:
     #   [bs, seq_len, num_attention_heads * attention_head_size = hidden_size].
-    attention_logits = query @ torch.transpose(key, -2, -1)
-    attention_logits = attention_logits / torch.sqrt(Tensor([self.attention_head_size]))
-    attention = nn.Softmax(dim=-1)(attention_logits * attention_mask)
-    output = attention @ value
+    attention_scores = query @ torch.transpose(key, -2, -1) / torch.sqrt(Tensor([self.attention_head_size]))
+    normalized_attention_scores = self.dropout(nn.Softmax(dim=-1)(attention_scores + attention_mask))
+    output = normalized_attention_scores @ value
     output = torch.transpose(output, 1, 2)
     bs, seq_len = output.shape[:2]
     output = output.reshape(bs, seq_len, -1)